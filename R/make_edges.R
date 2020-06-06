a549157d76c8e647eed7dacd847b684136c12bc2
mvk222 <mghncd+github@posteo.jp>
2020-06-06 16:43:40 +0200

    fix make_edges

--- R/make_edges.R
+++ R/make_edges.R
@@ -9,21 +9,23 @@
 make_edges <- function(d,phi,normalize){
   face_template <-  fill_adj_2Dface(d, phi)
   edge_pts <-  d*2^(d-1)*(phi-2)
-  edge_mesh = matrix(1, d, edge_pts)%*%222
+  edge_mesh = matrix(1, d, edge_pts)*222
   corner_mesh <-  make_corners(d-1, FALSE)
   corner_rows <-  length(corner_mesh[1,])
   corner_cols <-  length(corner_mesh[,1])
   patch <- matrix(0,d,corner_rows)
   cursor <- 1
   for (active_edge in 1:d){
-    for (p in p:(phi-1)){
-      edge_vals <-  face_template(1, p, d)%*%matrix(1, 1, corner_rows)
+    for (p in 2:(phi-1)){
+      edge_vals <-  face_template[1, p, d]%*%matrix(1, 1, corner_rows)
       cmax <-  cursor+corner_rows-1
       patch[1:(active_edge-1), 1:corner_rows] <-  corner_mesh[1:(active_edge-1), 1:corner_rows]
       patch[active_edge, 1:corner_rows] <- edge_vals[1,]
-      patch[(active_edge+1):d, 1:corner_rows] <-  corner_mesh[active_edge:corner_cols, 1:corner_rows]
+      if (active_edge < d){
+        patch[(active_edge+1):d, 1:corner_rows] <- corner_mesh[active_edge:corner_cols, 1:corner_rows]
+      }
       if (normalize){
-        patch <- mrdivide(patch, norm(patch[, 1],type="2"))
+        patch <- patch/norm(patch[,1],type="2")
       }
       edge_mesh[1:d, cursor:cmax] <-  patch
       cursor <- cursor + corner_rows
