a6cc2e7af2c4983254a22311cd0824ccfb91b94c
mvk222 <mghncd+github@posteo.jp>
2020-06-06 16:14:13 +0200

    fix corn_mesh

--- R/make_corners.R
+++ R/make_corners.R
@@ -6,9 +6,9 @@
 #' @import pracma
 
 make_corners <- function(d,normalize){
-  corn_mesh <-  fill_corners(matrix(1,d,2^d)*111, matrix(0,d,1), 0, 1)
+  corn_mesh <-  fill_corners(matrix(1,d,2^d)*111, matrix(0,d,1), 0, 1)[[1]]
   # Scale every point to unit length:
   scal <-  norm(corn_mesh[, 1],type="2")
-  corn_mesh <-  mrdivide(corn_mesh,scal)
+  corn_mesh <-  corn_mesh/scal
   return(corn_mesh)
 }
